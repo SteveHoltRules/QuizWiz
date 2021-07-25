81a7e8febd425b9f5cf4401eec66615157a4ddf4
Matt Luna <mat.c.luna@gmail.com>
2021-07-25 00:29:54 -0500

    testing on changes to quiz

--- Assets/script2.js
+++ Assets/script2.js
@@ -14,24 +14,32 @@ function buildQuiz() {
       for (letter in currentQuestion.answers) {
 
         // ...add an HTML radio button
+        // bootstrap has a seperatation from the label and the input whereas the example had an input only. How do I make this work with bootstrap?
+
         answers.push(
-          `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
+          `<div class="form-check">
+            <input class="form-check-input" type="radio" name="question${questionNumber}" id="flexRadioDefault1">
+            <label class="form-check-label" for="flexRadioDefault1" value="${letter}">
             ${letter} :
             ${currentQuestion.answers[letter]}
-          </label>`
+          </label>
+          </input>
+          </div>`
         );
       }
-
       // add this question and its answers to the output
+      //${placeholder values} Template literals and multi-line and substitution for what is passed}
+      //This is where the current question is passed and should be manipulated - alter the bootstrap here
       output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
+        `<div class="mt-1 ml-2 question"> ${currentQuestion.question} </div>
         <div class="answers"> ${answers.join('')} </div>`
+
       );
     }
   );
 
   // finally combine our output list into one string of HTML and put it on the page
+  //fix the innerHTML and substitute if for something better....
   quizContainer.innerHTML = output.join('');
 }
 
@@ -48,8 +56,11 @@ function showResults() {
 
     // find selected answer
     const answerContainer = answerContainers[questionNumber];
-    const selector = `input[name=question${questionNumber}]:checked`;
-    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+    const selector = `form-check-input[name=question${questionNumber}]:checked`;
+    console.log(selector);
+    const userAnswer = (answerContainer.querySelectorAll(selector) || {}).value;
+    //This is broken
+    console.log(userAnswer);
 
     // if answer is correct
     if (userAnswer === currentQuestion.correctAnswer) {
